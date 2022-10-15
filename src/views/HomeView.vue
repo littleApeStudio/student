4a229433463c686710c469cfa6c293e5a87f0d5b
LiuTeng <1714037953@qq.com>
2022-10-15 18:23:51 +0800

    form表单

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,99 @@
 <template>
   <div class="login_box">
-    <div class="login"></div>
+    <div class="login">
+      <el-form
+        label-position="left"
+        :model="userData"
+        status-icon
+        :rules="rules"
+        ref="userData"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="年龄" prop="username">
+          <el-input v-model.number="userData.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input
+            type="password"
+            v-model="userData.pass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input
+            type="password"
+            v-model="userData.checkPass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('userData')"
+            >提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    var checkusername = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("用户名不能为空"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.userData.checkPass !== "") {
+          this.$refs.userData.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.userData.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      userData: {
+        username: "",
+        pass: "",
+        checkPass: "",
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        username: [{ validator: checkusername, trigger: "blur" }],
+      },
+    };
   },
-  created(){
-    
-  }
-}
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 <style scoped>
 .login_box {
@@ -28,7 +105,7 @@ export default {
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
   width: 450px;
   height: 600px;
   background: #ffffff;
