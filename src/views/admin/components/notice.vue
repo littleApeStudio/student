cacc5575970a1af092c773cc0b1baa169fca2a98
LiuTeng <1714037953@qq.com>
2022-10-18 18:16:31 +0800

    管理员全部界面创建

---
+++ src/views/admin/components/notice.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "index",
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+div {
+  width: 200px;
+  height: 200px;
+  background: #fff;
+}
+</style>
--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -23,8 +23,8 @@
             <span slot="title">用户管理</span>
           </template>
           <el-menu-item-group>
-            <el-menu-item index="1-1">教师用户</el-menu-item>
-            <el-menu-item index="1-2">学生用户</el-menu-item>
+            <el-menu-item index="teacher">教师管理</el-menu-item>
+            <el-menu-item index="student">学生管理</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="2">
@@ -33,10 +33,10 @@
             <span slot="title">学院管理</span>
           </template>
           <el-menu-item-group>
-            <el-menu-item index="2-1">科目管理</el-menu-item>
-            <el-menu-item index="2-2">班级管理</el-menu-item>
-            <el-menu-item index="2-3">课程管理</el-menu-item>
-            <el-menu-item index="2-4">试题管理</el-menu-item>
+            <el-menu-item index="kemu">科目管理</el-menu-item>
+            <el-menu-item index="classs">班级管理</el-menu-item>
+            <el-menu-item index="course">课程管理</el-menu-item>
+            <el-menu-item index="shiti">试题管理</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
         <el-submenu index="3">
@@ -45,8 +45,8 @@
             <span slot="title">其他管理</span>
           </template>
           <el-menu-item-group>
-            <el-menu-item index="3-1">编写通知</el-menu-item>
-            <el-menu-item index="3-2">成绩统计</el-menu-item>
+            <el-menu-item index="notice">编写通知</el-menu-item>
+            <el-menu-item index="grade">成绩统计</el-menu-item>
           </el-menu-item-group>
         </el-submenu>
       </el-menu>
@@ -60,14 +60,52 @@
           ></i>
         </div>
       </div>
+      <div class="content">
+        <component :is="nowPage" :nowPage.sync="nowPage"></component>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import index from "./components/index.vue"; //首页
+import teacher from "./components/teacher.vue"; //教师管理
+import student from "./components/student.vue"; //学生管理
+import kemu from "./components/kemu.vue"; //科目管理
+import classs from "./components/class.vue"; //班级管理
+import course from "./components/course.vue"; //课程管理
+import shiti from "./components/shiti.vue"; //试题管理
+import notice from "./components/notice.vue"; //通知管理
+import grade from "./components/grade.vue"; //成绩统计
 export default {
+  components: {
+    index, //首页
+    teacher, //教师管理
+    student, //学生管理
+    kemu, //科目管理
+    classs, //班级管理
+    course, //课程管理
+    shiti, //试题管理
+    notice, //通知管理
+    grade, //成绩统计
+  },
   data() {
     return {
+      // 导航栏收缩
       isCollapse: false,
+      // 当前页面
+      nowPage: "index",
+      // 全部页面
+      allPages: [
+        "index",
+        "teacher",
+        "student",
+        "kemu",
+        "classs",
+        "course",
+        "shiti",
+        "notice",
+        "grade",
+      ],
     };
   },
   methods: {
@@ -153,9 +191,34 @@ export default {
 .admin .right .nav .menuopen:hover {
   color: #409eff;
 }
+.admin .right .content {
+  width: 100%;
+  height: calc(100% - 56px);
+  overflow-x: hidden;
+  overflow-y: auto;
+}
 </style>
 <style>
 body {
   background: #f7f7f7;
+}
+::-webkit-scrollbar {
+  width: 4px;
+}
+::-webkit-scrollbar-thumb {
+  background-color: #dddddd;
+  border-radius: 2px;
+}
+::-webkit-scrollbar-track {
+  background-color: none;
+}
+::-webkit-scrollbar-thumb:horizontal {
+  background-color: none;
+}
+::-webkit-scrollbar-track:horizontal {
+  background-color: none;
+}
+::-webkit-scrollbar-corner {
+  background-color: none;
 }
 </style>
